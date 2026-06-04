51032049514ce48b801f643d022c250ff315100d
smexyvanilla <muskancv1004@gmail.com>
2026-06-04 12:14:32 +0530

    'setup'

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT = '-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.strip() for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
+setup(
+    name='MLProject',
+    version='0.0.1',
+    author='Muskan',
+    author_email='muskancv1004@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
