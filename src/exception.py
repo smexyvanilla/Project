fa2578914198a496a90b1b19472385d64c23b8c7
smexyvanilla <muskancv1004@gmail.com>
2026-06-04 17:17:13 +0530

    Added logger and custom exception

---
+++ src/exception.py
@@ -0,0 +1,36 @@
+import sys
+from src.logger import logging 
+
+def error_message_details(error, error_detail):
+    _, _, exc_tb = error_detail.exc_info()
+
+    file_name = exc_tb.tb_frame.f_code.co_filename
+
+    error_message = (
+        f"Error occurred in python script [{file_name}] "
+        f"at line number [{exc_tb.tb_lineno}] "
+        f"error message [{error}]"
+    )
+
+    return error_message
+
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail):
+        super().__init__(error_message)
+
+        self.error_message = error_message_details(
+            error_message,
+            error_detail
+        )
+
+    def __str__(self):
+        return self.error_message
+
+
+'''if __name__ == "__main__":
+    try:
+        a=4/0
+    except Exception as e:
+        logging.info("Division by zero")
+        raise CustomException(e, sys)>>>>>>>>>>>TO TESTIFY THE WORKING'''
